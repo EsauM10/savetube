327d9f1225ac6deecc528d42a9742edadf62bdb4
EsauM10 <esau2096@gmail.com>
2024-12-15 21:04:37 -0300

    chore: add oxygenio package

--- web/src/hooks/useOxygen.tsx
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client";
-
-const url = `http://localhost:15999`;
-const socket = io(url);
-
-window.addEventListener("beforeunload", () => socket.emit("onclose"));
-
-export { socket };
